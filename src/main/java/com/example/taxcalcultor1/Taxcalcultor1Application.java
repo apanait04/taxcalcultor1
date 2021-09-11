5be871395f82c8f49909ac70c6cf7dd049cb17c7
andreip <andrei.panait98@gmail.com>
2021-09-11 13:27:44 +0300

    spring scheleton

---
+++ src/main/java/com/example/taxcalcultor1/Taxcalcultor1Application.java
@@ -0,0 +1,13 @@
+package com.example.taxcalcultor1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taxcalcultor1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taxcalcultor1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taxcalcultor1/Taxcalcultor1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxcalcultor1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taxcalcultor1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
