b4dfe6df9ecd04487fca359ab345af2ff617a1de
andreip <andrei.panait98@gmail.com>
2021-09-15 13:42:23 +0300

    user registartion not working

---
+++ src/main/java/app/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package app.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
